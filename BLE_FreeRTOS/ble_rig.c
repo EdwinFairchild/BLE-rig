da905ae6429d0ae4600bcbcc9547b982d417c23e
Eddie <eddie.amaya@analog.com>
2023-01-06 16:09:49 -0600

    move char writeback handler to ble_rig.c

--- BLE_FreeRTOS/ble_rig.c
+++ BLE_FreeRTOS/ble_rig.c
@@ -5,7 +5,6 @@
 #define RED_LED_PIN MXC_GPIO_PIN_OUT
 #define RED_LED_PORT MXC_GPIO_PORT_OUT
 
-
 /* Mutual exclusion (mutex) semaphores */
 SemaphoreHandle_t xGPIOmutex;
 mxc_gpio_cfg_t gpio_out;
@@ -21,10 +20,10 @@ TaskHandle_t vTask1_hdl;
  *  \return None.
  */
 /*************************************************************************************************/
-uint32_t crc32_for_byte(uint32_t r)
-{
-    for (int j = 0; j < 8; ++j) r = (r & 1 ? 0 : (uint32_t)0xEDB88320L) ^ r >> 1;
-    return r ^ (uint32_t)0xFF000000L;
+uint32_t crc32_for_byte(uint32_t r) {
+  for (int j = 0; j < 8; ++j)
+    r = (r & 1 ? 0 : (uint32_t)0xEDB88320L) ^ r >> 1;
+  return r ^ (uint32_t)0xFF000000L;
 }
 
 /*************************************************************************************************/
@@ -38,79 +37,106 @@ uint32_t crc32_for_byte(uint32_t r)
  *  \return None.
  */
 /*************************************************************************************************/
-static uint32_t table[0x100] = { 0 };
-void crc32(const void *data, size_t n_bytes, uint32_t *crc)
-{
-    if (!*table) {
-        for (size_t i = 0; i < 0x100; ++i) table[i] = crc32_for_byte(i);
-    }
-    for (size_t i = 0; i < n_bytes; ++i) {
-        *crc = table[(uint8_t)*crc ^ ((uint8_t *)data)[i]] ^ *crc >> 8;
-    }
+static uint32_t table[0x100] = {0};
+void crc32(const void *data, size_t n_bytes, uint32_t *crc) {
+  if (!*table) {
+    for (size_t i = 0; i < 0x100; ++i)
+      table[i] = crc32_for_byte(i);
+  }
+  for (size_t i = 0; i < n_bytes; ++i) {
+    *crc = table[(uint8_t)*crc ^ ((uint8_t *)data)[i]] ^ *crc >> 8;
+  }
 }
 
-void config_gpio(void)
-{
-   
-    /* Setup output pin. */
-    gpio_out.port = RED_LED_PORT;
-    gpio_out.mask = RED_LED_PIN;
-    gpio_out.pad = MXC_GPIO_PAD_NONE;
-    gpio_out.func = MXC_GPIO_FUNC_OUT;
-    gpio_out.vssel = MXC_GPIO_VSSEL_VDDIOH;
-    MXC_GPIO_Config(&gpio_out);
+void config_gpio(void) {
+
+  /* Setup output pin. */
+  gpio_out.port = RED_LED_PORT;
+  gpio_out.mask = RED_LED_PIN;
+  gpio_out.pad = MXC_GPIO_PAD_NONE;
+  gpio_out.func = MXC_GPIO_FUNC_OUT;
+  gpio_out.vssel = MXC_GPIO_VSSEL_VDDIOH;
+  MXC_GPIO_Config(&gpio_out);
 }
 
-void vTask1(void *pvParameters)
-{
-    TickType_t xLastWakeTime;
-    unsigned int x = LED_ON;
+void vTask1(void *pvParameters) {
+  TickType_t xLastWakeTime;
+  unsigned int x = LED_ON;
 
-    /* Get task start time */
-    xLastWakeTime = xTaskGetTickCount();
-    uint32_t notificationValue;
-    bool state = false;
-    while (1) {
-        xTaskNotifyWait( 0x00, 0xFFFFFFFF, &notificationValue,  portMAX_DELAY );
-        APP_TRACE_INFO3("ME14: %d\r\nME17: %d \r\nME18: %d", (notificationValue&0x01),(notificationValue&0x02),(notificationValue&0x03));
-        
-        if(state == true){
-        MXC_GPIO_OutSet(gpio_out.port, gpio_out.mask);
-        state = false;
-        }
-        else{
-            MXC_GPIO_OutClr(gpio_out.port, gpio_out.mask);
-        state = true;
-        }
+  /* Get task start time */
+  xLastWakeTime = xTaskGetTickCount();
+  uint32_t notificationValue;
+  bool state = false;
+  while (1) {
+    xTaskNotifyWait(0x00, 0xFFFFFFFF, &notificationValue, portMAX_DELAY);
+    APP_TRACE_INFO3("ME14: %d\r\nME17: %d \r\nME18: %d",
+                    (notificationValue & 0x01), (notificationValue & 0x02),
+                    (notificationValue & 0x03));
 
-
-
-        // if (xSemaphoreTake(xGPIOmutex, portMAX_DELAY) == pdTRUE) {
-           
-        //     /* Return the mutex after we have modified the hardware state */
-        //     xSemaphoreGive(xGPIOmutex);
-        // }
- 
-        
+    if (state == true) {
+      MXC_GPIO_OutSet(gpio_out.port, gpio_out.mask);
+      state = false;
+    } else {
+      MXC_GPIO_OutClr(gpio_out.port, gpio_out.mask);
+      state = true;
     }
+
+    // if (xSemaphoreTake(xGPIOmutex, portMAX_DELAY) == pdTRUE) {
+
+    //     /* Return the mutex after we have modified the hardware state */
+    //     xSemaphoreGive(xGPIOmutex);
+    // }
+  }
 }
 
-void freeRTOS_init(void)
-{
-    /* Create mutexes */
-    xGPIOmutex = xSemaphoreCreateMutex();
-    if (xGPIOmutex == NULL) {
-        APP_TRACE_INFO0("xSemaphoreCreateMutex failed to create a mutex.\n");
+uint8_t datsWpWriteCback(dmConnId_t connId, uint16_t handle, uint8_t operation,
+                         uint16_t offset, uint16_t len, uint8_t *pValue,
+                         attsAttr_t *pAttr) {
+  powerOptions_t temp;
+  uint32_t crcResult = 0x00000000;
+  int err = 0;
+  if (len == sizeof(powerOptions_t)) {
+    memcpy(&temp, pValue, sizeof(powerOptions_t));
+    // calculate crc32 starting from second element
+    crc32(&temp.packet_type, sizeof(powerOptions_t) - 4, &crcResult);
+    if (crcResult == temp.crc32) {
+      // process message
+      uint32_t valueToSend =
+          (temp.me14_state << 0 | temp.me17_state << 1 | temp.me18_state << 2);
+      xTaskNotify(vTask1_hdl, valueToSend, eSetValueWithOverwrite);
+
+    } else {
+      // TODO implement retry
+      APP_TRACE_INFO0("CRC did not mathc");
+      err++;
     }
 
-    //start tasks
-     if (xTaskCreate(vTask1, (const char *)"Task1", configMINIMAL_STACK_SIZE, NULL,tskIDLE_PRIORITY + 1, &vTask1_hdl) != pdPASS)
-    {
-        APP_TRACE_INFO0("Failed to create task");
+  } else {
+    APP_TRACE_INFO0("Len did not match");
+  }
+  // if (len < 64) {
+  //     /* print received data if not a speed test message */
+  //     APP_TRACE_INFO0((const char *)pValue);
 
-    }
-    else{
-        APP_TRACE_INFO0("Task 1 created!");
-    } 
+  //     /* send back some data */
+  //     datsSendData(connId);
+  // }
+  return ATT_SUCCESS;
+}
 
+void freeRTOS_init(void) {
+  /* Create mutexes */
+  xGPIOmutex = xSemaphoreCreateMutex();
+  if (xGPIOmutex == NULL) {
+    APP_TRACE_INFO0("xSemaphoreCreateMutex failed to create a mutex.\n");
+  }
+
+  // start tasks
+  if (xTaskCreate(vTask1, (const char *)"Task1", configMINIMAL_STACK_SIZE, NULL,
+                  tskIDLE_PRIORITY + 1, &vTask1_hdl) != pdPASS) {
+    APP_TRACE_INFO0("Failed to create task");
+
+  } else {
+    APP_TRACE_INFO0("Task 1 created!");
+  }
 }
--- BLE_FreeRTOS/ble_rig.h
+++ BLE_FreeRTOS/ble_rig.h
@@ -1,63 +1,82 @@
 #ifndef _BLE_RIG_H_
 #define _BLE_RIG_H_
 
-#include <stdint.h>
-#include <stddef.h>
-#include <stdio.h>
-#include <string.h>
+#include "board.h"
+#include "gpio.h"
 #include "mxc_device.h"
 #include "nvic_table.h"
 #include "pb.h"
-#include "board.h"
-#include "gpio.h"
-
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
 #include <string.h>
-#include "wsf_types.h"
-#include "util/bstream.h"
-#include "wsf_msg.h"
-#include "wsf_trace.h"
-#include "wsf_buf.h"
-#include "wsf_nvm.h"
-#include "wsf_timer.h"
-#include "hci_api.h"
-#include "sec_api.h"
-#include "dm_api.h"
-#include "smp_api.h"
-#include "att_api.h"
-#include "app_api.h"
-#include "app_main.h"
-#include "app_db.h"
-#include "app_ui.h"
-#include "svc_ch.h"
-#include "svc_core.h"
-#include "svc_wp.h"
-#include "util/calc128.h"
-#include "gatt/gatt_api.h"
-#include "dats_api.h"
-#include "wut.h"
-#include "trimsir_regs.h"
-#include "pal_btn.h"
-#include "pal_uart.h"
-#include "tmr.h"
-#include "svc_sds.h"
-#include "FreeRTOSConfig.h"
+
 #include "FreeRTOS.h"
 #include "FreeRTOSConfig.h"
-#include "queue.h"
-#include "portmacro.h"
-#include "task.h"
-#include "semphr.h"
 #include "FreeRTOS_CLI.h"
-#include "mxc_device.h"
-#include "wut.h"
-#include "uart.h"
-#include "lp.h"
-#include "led.h"
+#include "app_api.h"
+#include "app_db.h"
+#include "app_main.h"
+#include "app_ui.h"
+#include "att_api.h"
 #include "board.h"
+#include "dats_api.h"
+#include "dm_api.h"
+#include "gatt/gatt_api.h"
+#include "hci_api.h"
+#include "led.h"
+#include "lp.h"
+#include "mxc_device.h"
+#include "pal_btn.h"
+#include "pal_uart.h"
+#include "portmacro.h"
+#include "queue.h"
+#include "sec_api.h"
+#include "semphr.h"
+#include "smp_api.h"
+#include "svc_ch.h"
+#include "svc_core.h"
+#include "svc_sds.h"
+#include "svc_wp.h"
+#include "task.h"
+#include "tmr.h"
+#include "trimsir_regs.h"
+#include "uart.h"
+#include "util/bstream.h"
+#include "util/calc128.h"
+#include "wsf_buf.h"
+#include "wsf_msg.h"
+#include "wsf_nvm.h"
+#include "wsf_timer.h"
+#include "wsf_trace.h"
+#include "wsf_types.h"
+#include "wut.h"
+#include <string.h>
+
+typedef enum __attribute__((__packed__)) {
+  POWER_OPTIONS,
+  RESERVED
+
+} packet_t;
+
+typedef enum __attribute__((__packed__)) { OFF, ON } power_state_t;
+
+typedef struct __attribute__((__packed__)) {
+  uint32_t crc32;
+  packet_t packet_type;
+  power_state_t me14_state;
+  power_state_t me17_state;
+  power_state_t me18_state;
+  uint32_t magicNum;
+
+} powerOptions_t;
 
 uint32_t crc32_for_byte(uint32_t r);
 void crc32(const void *data, size_t n_bytes, uint32_t *crc);
 void config_gpio(void);
 void vTask1(void *pvParameters);
+uint8_t datsWpWriteCback(dmConnId_t connId, uint16_t handle, uint8_t operation,
+                         uint16_t offset, uint16_t len, uint8_t *pValue,
+                         attsAttr_t *pAttr);
 void freeRTOS_init(void);
 #endif
