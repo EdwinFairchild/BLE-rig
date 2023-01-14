29960ab9e19205f92f75332c678910a5081b421a
Eddie <eddie@edwinfairchild.com>
2023-01-13 19:17:36 -0600

    me17 serial command

--- BLE_dats/ble_rig.c
+++ BLE_dats/ble_rig.c
@@ -13,16 +13,15 @@
 #define ME18_PIN MXC_GPIO_PIN_8
 
 powerOptions_t power_ctl;
-
+uint8_t hanlderId = 0;
 /*! \brief    Custom Command Handler. */
-static uint8_t appNameCommandHandler(uint32_t argc, char **argv);
-
+static uint8_t cmd_me17MainState(uint32_t argc, char **argv);
 
 /*! \brief    command list */
 terminalCommand_t appTerminalCustomCommandList[] = {
 
-    { NULL, "name", "name <First> <Last>", appNameCommandHandler },
-    { NULL, NULL, NULL, NULL }, // used as delimter for end of list
+    {NULL, "me17main", "<power state>>", cmd_me17MainState},
+    {NULL, NULL, NULL, NULL}, // used as delimter for end of list
 
 };
 // http://home.thep.lu.se/~bjorn/crc/
@@ -35,10 +34,10 @@ terminalCommand_t appTerminalCustomCommandList[] = {
  *  \return None.
  */
 /*************************************************************************************************/
-uint32_t crc32_for_byte(uint32_t r)
-{
-    for (int j = 0; j < 8; ++j) r = (r & 1 ? 0 : (uint32_t)0xEDB88320L) ^ r >> 1;
-    return r ^ (uint32_t)0xFF000000L;
+uint32_t crc32_for_byte(uint32_t r) {
+  for (int j = 0; j < 8; ++j)
+    r = (r & 1 ? 0 : (uint32_t)0xEDB88320L) ^ r >> 1;
+  return r ^ (uint32_t)0xFF000000L;
 }
 
 /*************************************************************************************************/
@@ -52,15 +51,15 @@ uint32_t crc32_for_byte(uint32_t r)
  *  \return None.
  */
 /*************************************************************************************************/
-static uint32_t table[0x100] = { 0 };
-void crc32(const void *data, size_t n_bytes, uint32_t *crc)
-{
-    if (!*table) {
-        for (size_t i = 0; i < 0x100; ++i) table[i] = crc32_for_byte(i);
-    }
-    for (size_t i = 0; i < n_bytes; ++i) {
-        *crc = table[(uint8_t)*crc ^ ((uint8_t *)data)[i]] ^ *crc >> 8;
-    }
+static uint32_t table[0x100] = {0};
+void crc32(const void *data, size_t n_bytes, uint32_t *crc) {
+  if (!*table) {
+    for (size_t i = 0; i < 0x100; ++i)
+      table[i] = crc32_for_byte(i);
+  }
+  for (size_t i = 0; i < n_bytes; ++i) {
+    *crc = table[(uint8_t)*crc ^ ((uint8_t *)data)[i]] ^ *crc >> 8;
+  }
 }
 
 mxc_gpio_cfg_t gpio_out;
@@ -85,6 +84,58 @@ void config_gpio(void) {
   MXC_GPIO_Config(&gpio_out);
   APP_TRACE_INFO1("Size of enum %d", sizeof(packet_t));
 }
+
+void parsePacket() {
+
+  if (power_ctl.all_on == true) {
+    MXC_GPIO_OutSet(ME17_MAIN_PORT, ME17_MAIN_PIN);
+    MXC_GPIO_OutSet(ME17_PORT, ME17_PIN);
+    MXC_GPIO_OutSet(ME14_PORT, ME14_PIN);
+    MXC_GPIO_OutSet(ME18_PORT, ME18_PIN);
+    APP_TRACE_INFO0("All on");
+
+  } else if (power_ctl.all_off == true) {
+    MXC_GPIO_OutClr(ME17_MAIN_PORT, ME17_MAIN_PIN);
+    MXC_GPIO_OutClr(ME17_PORT, ME17_PIN);
+    MXC_GPIO_OutClr(ME14_PORT, ME14_PIN);
+    MXC_GPIO_OutClr(ME18_PORT, ME18_PIN);
+    APP_TRACE_INFO0("All off");
+
+  } else {
+
+    if (power_ctl.me17_main_state) {
+      APP_TRACE_INFO0("ME17 Main turned on");
+      MXC_GPIO_OutSet(ME17_MAIN_PORT, ME17_MAIN_PIN);
+    } else {
+      APP_TRACE_INFO0("ME17 Main turned off");
+      MXC_GPIO_OutClr(ME17_MAIN_PORT, ME17_MAIN_PIN);
+    }
+
+    if (power_ctl.me17_state) {
+      APP_TRACE_INFO0("ME17 turned on");
+      MXC_GPIO_OutSet(ME17_PORT, ME17_PIN);
+    } else {
+      APP_TRACE_INFO0("ME17 turned off");
+      MXC_GPIO_OutClr(ME17_PORT, ME17_PIN);
+    }
+
+    if (power_ctl.me14_state) {
+      APP_TRACE_INFO0("ME14 turned on");
+      MXC_GPIO_OutSet(ME14_PORT, ME14_PIN);
+    } else {
+      APP_TRACE_INFO0("ME14 turned off");
+      MXC_GPIO_OutClr(ME14_PORT, ME14_PIN);
+    }
+
+    if (power_ctl.me18_state) {
+      APP_TRACE_INFO0("ME18 turned on");
+      MXC_GPIO_OutSet(ME18_PORT, ME18_PIN);
+    } else {
+      APP_TRACE_INFO0("ME18 turned off");
+      MXC_GPIO_OutClr(ME18_PORT, ME18_PIN);
+    }
+  }
+}
 /*************************************************************************************************/
 /*!
  *  \brief  Handler for a custom terminal command.
@@ -95,24 +146,34 @@ void config_gpio(void) {
  *  \return Error code.
  */
 /*************************************************************************************************/
-static uint8_t appNameCommandHandler(uint32_t argc, char **argv)
-{
-    if (argc < 3) {
-        return TERMINAL_ERROR_TOO_FEW_ARGUMENTS;
-    } else if (argc == 3) {
-        TerminalTxPrint("Hello %s %s\r\n", argv[1], argv[2]);
+static uint8_t cmd_me17MainState(uint32_t argc, char **argv) {
+  dmEvt_t *pMsg;
+  pMsg = WsfMsgAlloc(sizeof(attEvt_t));
+  pMsg->hdr.event = DM_VENDOR_SPEC_IND;
 
+  if (argc < 2) {
+    return TERMINAL_ERROR_TOO_FEW_ARGUMENTS;
+  } else if (argc == 2) {
+    if (atoi(argv[1]) == 1) {
+      APP_TRACE_INFO0("tunred on ecieved");
+      power_ctl.me17_main_state = ON;
     } else {
-        return TERMINAL_ERROR_TOO_MANY_ARGUMENTS;
+      APP_TRACE_INFO0("tunred off ecieved");
+      power_ctl.me17_main_state = OFF;
     }
+    WsfMsgSend(hanlderId, pMsg);
+  } else {
+    return TERMINAL_ERROR_TOO_MANY_ARGUMENTS;
+  }
 
-    return TERMINAL_ERROR_OK;
+  return TERMINAL_ERROR_OK;
 }
-void register_at_Commands(void)
-{
-    uint8_t index = 0;
-    while (appTerminalCustomCommandList[index].pName != NULL) {
-        TerminalRegisterCommand(&appTerminalCustomCommandList[index]);
-        index++;
-    }
+
+void register_at_Commands(void) {
+  uint8_t index = 0;
+  while (appTerminalCustomCommandList[index].pName != NULL) {
+    TerminalRegisterCommand(&appTerminalCustomCommandList[index]);
+    index++;
+  }
 }
+void setHandlerID(uint8_t id) { hanlderId = id; }
